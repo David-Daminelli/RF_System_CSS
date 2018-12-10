efe4001837db54cdf1ebb13217e84d815af63bb1
David Daminelli <Daviddaminelli@gmail.com>
2018-12-10 08:27:05 -0200

    Removed files
    Deleted useless files

--- CSS/teste.py
+++
@@ -1,7 +0,0 @@
-from org.csstudio.opibuilder.scriptUtil import PVUtil
-from org.csstudio.opibuilder.scriptUtil import ColorFontUtil
-from org.eclipse.jface.dialogs import MessageDialog
-from java.lang import Math
-print 'oi'
-
-
